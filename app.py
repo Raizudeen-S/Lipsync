5c9a725685de85dff72e21c1acd98cf77cdd51db
Raizudeen S <raizudeen.shameem@sifycorp.com>
2024-12-10 14:36:52 +0530

    Deleted Unnecessart files

--- app.py
+++ app.py
@@ -1,9 +1,8 @@
 import json
-from multiprocessing import Process
+# from multiprocessing import Process
 import subprocess, platform
 from inference_musetalk import inference
-from subfunctions.audio_process import tts, audio_output_from_video, is_speech, empty_folder
-from moviepy.editor import VideoFileClip, AudioFileClip
+import subfunctions.audio_process as AP
 from flask import Flask, request, jsonify, session, send_from_directory
 import os, shutil
 from flask_cors import CORS
@@ -17,7 +16,7 @@ UPLOAD_FOLDER = "inputs/input_video/video.mp4"
 app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
 audio_file_path = "inputs/input_audio/audio.wav"
 process_running = False
-
+ 
 with open("inputs/voices.json", "r") as file:
     data = json.load(file)
     male_images = data["thumbnail"]["male_images"]
@@ -25,42 +24,16 @@ with open("inputs/voices.json", "r") as file:
     male_voice = data["voices"]["male_voice"]
     female_voice = data["voices"]["female_voice"]
 
-
-def is_video_audio_same(video_path, audio_path):
-    # Load video and get duration
-    video_clip = VideoFileClip(video_path)
-    video_duration = video_clip.duration
-    video_clip.close()
-
-    # Load audio and get duration
-    audio_clip = AudioFileClip(audio_path)
-    audio_duration = audio_clip.duration
-    audio_clip.close()
-
-    # Print durations for debugging
-    print(f"Video duration: {video_duration:.2f} seconds")
-    print(f"Audio duration: {audio_duration:.2f} seconds")
-
-    # Check if audio duration matches the video duration criteria
-    if video_duration >= audio_duration >= (video_duration - 5):
-        return True, video_duration, audio_duration
-
-    return False, video_duration, audio_duration
-
-
-# Generate a random secret key
 app.secret_key = os.urandom(24)
-
+ 
 @app.route("/inputs/<path:filename>")
 def serves_static(filename):
     return app.send_static_file(filename)
-
 
 @app.route("/results/<path:filename>")
 def serve_static(filename):
     print(filename)
     return send_from_directory(app.root_path + "/results/", filename)
-
 
 @app.route("/video_upload", methods=["POST"])
 def upload_file():
@@ -71,15 +44,14 @@ def upload_file():
         return jsonify({"message": "No selected file", "success": False}), 400
     if file:
         file.save(os.path.join(app.config["UPLOAD_FOLDER"]))
-        if is_speech(UPLOAD_FOLDER, audio_file_path):
+        if AP.is_speech(UPLOAD_FOLDER, audio_file_path):
             session["use_text"] = False
             return jsonify({"data": None, "success": True, "message": "Video has transcript", "text": True}), 200
         else:
             session["use_text"] = True
             return jsonify({"data": None, "success": True, "message": "No transcript is present", "text": False}), 200
     return jsonify({"message": "File upload failed", "success": False}), 400
-
-
+ 
 @app.route("/text_input", methods=["POST"])
 def text_input():
     text = request.json.get("text")  # Get the text from the request
@@ -90,10 +62,9 @@ def text_input():
         return jsonify({"data": {"text": text}, "success": True, "message": "Text input saved successfully"}), 200
     if audio_flag == False:
         session["use_text"] = False
-        audio_output_from_video(UPLOAD_FOLDER, audio_file_path)
+        AP.audio_output_from_video(UPLOAD_FOLDER, audio_file_path)
         return jsonify({"data": None, "success": True, "message": "Using Video Audio"}), 200
     return (jsonify({"message": "Text input is missing", "success": False}),)
-
 
 @app.route("/gender/<gender>", methods=["GET"])  # send text
 def choices(gender):
@@ -105,18 +76,14 @@ def choices(gender):
     elif gender == "female":
         return jsonify({"data": {"voices": female_voice, "images": female_images}, "success": True, "message": "Female is selected"}), 200
     return jsonify({"message": "Invalid gender", "success": False}), 400
-
-
+ 
 @app.route("/audio_gen", methods=["POST"])
 def audio_gen():
     voice = request.json.get("voice_choice")
     text = session.get("text")  # Retrieve text from session
     if voice and text:
-        tts_process = Process(target=tts, args=(text, voice, audio_file_path))
-        tts_process.start()
-        tts_process.join()
-        tts_process.terminate()
-        flag, video_duration, audio_duration = is_video_audio_same(UPLOAD_FOLDER, audio_file_path)
+        AP.tts(text, voice, audio_file_path)
+        flag, video_duration, audio_duration = AP.is_video_audio_same(UPLOAD_FOLDER, audio_file_path)
         if flag:
             return jsonify({"data": {"audio": audio_file_path}, "message": "Audio from Text generated successfully", "success": True}), 200
         else:
@@ -132,7 +99,6 @@ def audio_gen():
             )
     return jsonify({"message": "Text or voice is missing", "success": False}), 400
 
-
 @app.route("/preview", methods=["POST"])
 def preview_video_process():
     video_input = UPLOAD_FOLDER
@@ -140,21 +106,21 @@ def preview_video_process():
     avatar = request.json.get("image_choice")
     if not avatar:
         return jsonify({"message": "Avatar choice is missing"}), 400
-
+ 
     # Store video generation location in session
     session["video_gen_location"] = video_gen_location
-
+ 
     # Validate and process the avatar choice
     if "inputs/faces/thumbnails/" not in avatar:
         return jsonify({"message": "Invalid avatar choice"}), 400
-
+ 
     # Extract the base file name (e.g., male1 from inputs/faces/thumbnails/male1.png)
     avatar_name = avatar.split("/")[-1].split(".")[0]
-
+ 
     # Construct the preview video path and store it in the session
     preview_video = f"inputs/faces/{avatar_name}.mp4"
     session["selected_image"] = preview_video
-
+ 
     preview_output = "temp/preview.mp4"
 
     if video_gen_location == "Bottom Right":
@@ -194,8 +160,7 @@ def preview_video_process():
     subprocess.call(command, shell=platform.system() != "Windows")
 
     return jsonify({"data": {"result": preview_output}, "message": "Preview video generated successfully", "success": True}), 200
-
-
+ 
 @app.route("/generate", methods=["POST"])
 def generate_video_process():
     global process_running
@@ -204,73 +169,73 @@ def generate_video_process():
     avatar = request.json.get("image_choice")
     if not avatar:
         return jsonify({"message": "Avatar choice is missing"}), 400
-
+ 
     # Store video generation location in session
     session["video_gen_location"] = video_gen_location
-
+ 
     # Extract the base file name (e.g., male1 from inputs/faces/thumbnails/male1.png)
     avatar_name = avatar.split("/")[-1].split(".")[0]
-
+ 
     # Construct the preview video path and store it in the session
     preview_video = f"inputs/faces/{avatar_name}.mp4"
     session["selected_image"] = preview_video
-
+ 
     try:
         video_input = UPLOAD_FOLDER
         selected_Video = session.get("selected_image")
         enchance_video_output = "inputs/input_video/outputxxx_men1_audio.mp4"
         final_output = "results/final_result.mp4"
-
+ 
         enchance_video_output = inference(selected_Video,audio_file_path, 0)
-
+ 
         if video_gen_location == "Bottom Right":
-            command = """ffmpeg -i {} -i {} -filter_complex "[0:v]scale=1920:1080[first]; [1:v]scale=1920:1080[second]; [second]colorkey=0x00FF00:0.4:0.05[cleaned]; [cleaned]scale=iw/2.5:ih/2.5[scaled];
+            command = """ffmpeg -i {} -i {} -filter_complex "[0:v]scale=1920:1080[first]; [1:v]scale=1920:1080:force_original_aspect_ratio=decrease[second]; [second]colorkey=0x00FF00:0.4:0.05[cleaned]; [cleaned]scale=iw/2.5:ih/2.5[scaled];
                 [first][scaled]overlay=W-w--100:H-h" -preset veryslow -map 1:a -c:a copy {} -y""".format(
                 video_input, enchance_video_output, final_output
             )
         elif video_gen_location == "Bottom Left":
-            command = """ffmpeg -i {} -i {} -filter_complex "[0:v]scale=1920:1080[first]; [1:v]scale=1920:1080[second]; [second]colorkey=0x00FF00:0.4:0.05[cleaned]; [cleaned]scale=iw/2.5:ih/2.5[scaled];
+            command = """ffmpeg -i {} -i {} -filter_complex "[0:v]scale=1920:1080[first]; [1:v]scale=1920:1080:force_original_aspect_ratio=decrease[second]; [second]colorkey=0x00FF00:0.4:0.05[cleaned]; [cleaned]scale=iw/2.5:ih/2.5[scaled];
                 [first][scaled]overlay=-100:H-h" -preset veryslow -map 1:a -c:a copy {} -y""".format(
                 video_input, enchance_video_output, final_output
             )
         elif video_gen_location == "Top Right":
-            command = """ffmpeg -i {} -i {} -filter_complex "[0:v]scale=1920:1080[first]; [1:v]scale=1920:1080[second]; [second]colorkey=0x00FF00:0.4:0.05[cleaned]; [cleaned]scale=iw/2.5:ih/2.5[scaled];
+            command = """ffmpeg -i {} -i {} -filter_complex "[0:v]scale=1920:1080[first]; [1:v]scale=1920:1080:force_original_aspect_ratio=decrease[second]; [second]colorkey=0x00FF00:0.4:0.05[cleaned]; [cleaned]scale=iw/2.5:ih/2.5[scaled];
                 [first][scaled]overlay=W-w--100:0" -preset veryslow -map 1:a -c:a copy {} -y""".format(
                 video_input, enchance_video_output, final_output
             )
         elif video_gen_location == "Top Left":
-            command = """ffmpeg -i {} -i {} -filter_complex "[0:v]scale=1920:1080[first]; [1:v]scale=1920:1080[second]; [second]colorkey=0x00FF00:0.4:0.05[cleaned]; [cleaned]scale=iw/2.5:ih/2.5[scaled];
+            command = """ffmpeg -i {} -i {} -filter_complex "[0:v]scale=1920:1080[first]; [1:v]scale=1920:1080:force_original_aspect_ratio=decrease[second]; [second]colorkey=0x00FF00:0.4:0.05[cleaned]; [cleaned]scale=iw/2.5:ih/2.5[scaled];
                 [first][scaled]overlay=-100:0" -preset veryslow -map 1:a -c:a copy {} -y""".format(
                 video_input, enchance_video_output, final_output
             )
         elif video_gen_location == "Right":
-            command = """ffmpeg -i {} -i {} -filter_complex "[0:v]scale=1920:1080[first]; [1:v]scale=1920:1080[second]; [second]colorkey=0x00FF00:0.4:0.05[cleaned];
+            command = """ffmpeg -i {} -i {} -filter_complex "[0:v]scale=1920:1080[first]; [1:v]scale=1920:1080:force_original_aspect_ratio=decrease[second]; [second]colorkey=0x00FF00:0.4:0.05[cleaned];
                 [cleaned]scale=iw:ih[scaled];  [first][scaled]overlay=W-w/1.4:H-h" -preset veryslow -map 1:a -c:a copy {} -y""".format(
                 video_input, enchance_video_output, final_output
             )
         elif video_gen_location == "Left":
-            command = """ffmpeg -i {} -i {} -filter_complex "[0:v]scale=1920:1080[first]; [1:v]scale=1920:1080[second]; [second]colorkey=0x00FF00:0.4:0.05[cleaned];
+            command = """ffmpeg -i {} -i {} -filter_complex "[0:v]scale=1920:1080[first]; [1:v]scale=1920:1080:force_original_aspect_ratio=decrease[second]; [second]colorkey=0x00FF00:0.4:0.05[cleaned];
                 [cleaned]scale=iw:ih[scaled];  [first][scaled]overlay=-w/3.5:0" -preset veryslow -map 1:a -c:a copy {} -y""".format(
                 video_input, enchance_video_output, final_output
             )
         elif video_gen_location == "Center":
-            command = """ffmpeg -i {} -i {} -filter_complex "[0:v]scale=1920:1080[first]; [1:v]scale=1920:1080[second]; [second]colorkey=0x00FF00:0.4:0.05[cleaned];
+            command = """ffmpeg -i {} -i {} -filter_complex "[0:v]scale=1920:1080[first]; [1:v]scale=1920:1080:force_original_aspect_ratio=decrease[second]; [second]colorkey=0x00FF00:0.4:0.05[cleaned];
                 [cleaned]scale=iw:ih[scaled]; [first][scaled]overlay=(W-w)/2:(H-h)/2" -preset veryslow -map 1:a -c:a copy {} -y""".format(
                 video_input, enchance_video_output, final_output
             )
+ 
         else:
             return jsonify({"message": "Invalid choice"}), 400
-
+ 
         subprocess.call(command, shell=platform.system() != "Windows")
-
-        empty_folder("inputs/wav2lip_out")
-
+ 
+        AP.empty_folder("inputs/wav2lip_out")
+ 
         return jsonify({"data": {"result": final_output}, "message": "Preview video generated successfully", "success": True}), 200
-
+ 
     except Exception as e:
         print(e)
         return jsonify({"message": "Preview video generation failed", "success": False}), 400
-
-
+ 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=5000)
--- subfunctions/audio_process.py
+++ subfunctions/audio_process.py
@@ -1,42 +1,42 @@
 import edge_tts
-from moviepy.editor import VideoFileClip
+from moviepy.editor import VideoFileClip, AudioFileClip
 import speech_recognition as sr
 import os, shutil
-
-
+ 
+ 
 def tts(input_text, voice, audio_file_path):
     # Function to handle the TTS
     com = edge_tts.Communicate(input_text, voice)
     com.save_sync(audio_file_path)
     return audio_file_path
-
-
+ 
+ 
 def audio_output_from_video(video_input, audio_file_path):
     if not os.path.exists(video_input):
         return False
-
+ 
     video_clip = VideoFileClip(video_input)
     if video_clip.audio is None:
         print("No audio track found in the video.")
         return False
-
+ 
     audio_clip = video_clip.audio
     print(audio_clip, "audio-clipppppp")
     print(audio_file_path, "checkibgg-----")
     audio_clip.write_audiofile(audio_file_path)
-
+ 
     # Close the video and audio clip objects to free resources
     video_clip.close()
     audio_clip.close()
-
+ 
     return True
-
-
+ 
+ 
 def is_speech(video_input, audio_path):
     if audio_output_from_video(video_input, audio_path):
         # Initialize recognizer
         recognizer = sr.Recognizer()
-
+ 
         with sr.AudioFile(audio_path) as source:
             audio = recognizer.record(source)
         # Recognize speech using Google Web Speech API
@@ -49,8 +49,8 @@ def is_speech(video_input, audio_path):
             return True
     else:
         return False
-
-
+ 
+ 
 def empty_folder(folder):
     for filename in os.listdir(folder):
         file_path = os.path.join(folder, filename)
@@ -61,3 +61,25 @@ def empty_folder(folder):
                 shutil.rmtree(file_path)
         except Exception as e:
             print("Failed to delete %s. Reason: %s" % (file_path, e))
+ 
+def is_video_audio_same(video_path, audio_path):
+    # Load video and get duration
+    video_clip = VideoFileClip(video_path)
+    video_duration = video_clip.duration
+    video_clip.close()
+ 
+    # Load audio and get duration
+    audio_clip = AudioFileClip(audio_path)
+    audio_duration = audio_clip.duration
+    audio_clip.close()
+ 
+    # Print durations for debugging
+    print(f"Video duration: {video_duration:.2f} seconds")
+    print(f"Audio duration: {audio_duration:.2f} seconds")
+ 
+    # Check if audio duration matches the video duration criteria
+    if video_duration >= audio_duration >= (video_duration - 5):
+        return True, video_duration, audio_duration
+ 
+    return False, video_duration, audio_duration
+ 
